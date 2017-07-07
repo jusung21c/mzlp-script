b9e3486e2990c6d7ed9196f144a2d25920295b9f
JuicyJusung <jusung21c@gmail.com>
2017-07-07 14:40:53 +0900

    FR

---
+++ spec/mzlp-script-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MzlpScriptView from '../lib/mzlp-script-view';
+
+describe('MzlpScriptView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
